0f343eafd80a2440013c426ffeaa09b7a6df5001
Yhanahi Rosenthal <Yhanai@hotmail.com>
2022-11-17 00:01:00 -0300

    carrusel hecho

---
+++ src/main/resources/frontend/src/components/Carousel3.jsx
@@ -0,0 +1,54 @@
+import fries from '../img/fries.png';
+import onions from '../img/onionRings.png';
+import nuggets from '../img/nuggets.png';
+import '../css/carousel.css';
+
+
+function Carousel2(){
+    return(
+        <>
+        <div className="containerCarousel">
+            <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="false">
+  <div class="carousel-indicators">
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+  </div>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src={fries} class="d-block w-100 imgCarousel" alt="..." />
+      <div class="carousel-caption d-none d-md-block">
+        <h5 className='nameBurger'> Cerveza Corona</h5>
+        <p className='descriptionCarousel'>Dos refrescantes cervezas Corona de 750ml</p>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src={onions} class="d-block w-100 imgCarousel" alt="..." />
+      <div class="carousel-caption d-none d-md-block nameAndDescriptionOfImg">
+        <h5 className='nameBurger'>Cerveza Brahma</h5>
+        <p className='descriptionCarousel'>Lata de cerveza Brahma 900ml</p>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src={nuggets} class="d-block w-100 imgCarousel" alt="..." />
+      <div class="carousel-caption d-none d-md-block">
+        <h5 className='nameBurger'>Cerveza Stella Artois</h5>
+        <p className='descriptionCarousel'>Refrescante cerveza Stella Artois 610ml</p>
+      </div>
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
+        </div>
+        </>
+    )
+}
+
+export default Carousel2;
