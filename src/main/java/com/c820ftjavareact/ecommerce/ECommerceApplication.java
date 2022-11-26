1947a2b31323694f10cdd62aba2a5485336b1079
maite5 <maite_1humano@outlook.es>
2022-11-26 03:40:11 -0300

    add cloudinary pom main

--- src/main/java/com/c820ftjavareact/ecommerce/ECommerceApplication.java
+++ src/main/java/com/c820ftjavareact/ecommerce/ECommerceApplication.java
@@ -1,11 +1,19 @@
 package com.c820ftjavareact.ecommerce;
+import com.cloudinary.*;
+
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import javax.annotation.PostConstruct;
+import java.io.File;
+import java.io.IOException;
 import java.util.TimeZone;
+import com.cloudinary.utils.ObjectUtils;
+import io.github.cdimascio.dotenv.Dotenv;
+import org.springframework.util.ObjectUtils;
 
+import java.util.Map;
 @SpringBootApplication
 public class ECommerceApplication {
 	//@PostConstruct
@@ -13,8 +21,16 @@ public class ECommerceApplication {
 		//TimeZone.setDefault(TimeZone.getTimeZone("UTC-03:00"));
 	//}
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		SpringApplication.run(ECommerceApplication.class, args);
-	}
 
+	Cloudinary cloudinary = new Cloudinary(ObjectUtils.asMap(
+			"cloud_name", "my_cloud_name",
+			"api_key", "my_api_key",
+			"api_secret", "my_api_secret",
+			"secure", true));
+	File file = new File("my_image.jpg");
+	Map uploadResult = cloudinary.uploader().upload(file, ObjectUtils.emptyMap());
+
+}
 }
