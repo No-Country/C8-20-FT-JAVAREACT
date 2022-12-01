c61c2d0c3047905e54989971ab22d94b4547fece
Huichoman <rockbotica@gmail.com>
2022-12-01 13:57:37 -0600

    Jose, update vista principal

--- src/main/resources/frontend/src/App.jsx
+++ src/main/resources/frontend/src/App.jsx
@@ -3,6 +3,10 @@ import * as component from "./components";
 import Carousel from "./components/Home";
 import * as bootstrap from "bootstrap";
 
+import burgerIcon from "./assets/images/burger-menu-icon.png";
+import blackwood from "./assets/images/blackwood.jpg";
+import tomate from "./assets/images/tomate.png";
+import vegetales from "./assets/images/vegetales.png";
 import burger1 from "./assets/images/burgers/burger1.png";
 import burger2 from "./assets/images/burgers/burger2.png";
 import burger3 from "./assets/images/burgers/burger3.png";
@@ -25,14 +29,51 @@ function App() {
   ];
   return (
     <div className={styles.mainContainer}>
-      <component.Title />
-      <div className={styles.carruselContainer}>
-        <Carousel />
+      <div
+        className={styles.topContainer}
+        style={{
+          backgroundImage: `url(${blackwood})`,
+          backgroundSize: "cover",
+          backgroundRepeat: "no-repeat",
+          backgroundPosition: "center",
+        }}
+      >
+        <div className={styles.tomateContainer}>
+          <img src={tomate} alt="tomate"></img>
+        </div>
+        <div className={styles.vegetalesContainer}>
+          <img
+            className={styles.vegetalesImage}
+            src={vegetales}
+            alt="vegetales"
+          ></img>
+        </div>
+        <div className={styles.gradientLayer}>
+          <div className={styles.navbarContainer}>
+            <div className={styles.burgerIconContainer}>
+              <img
+                className={styles.burgerIconImage}
+                src={burgerIcon}
+                alt="burgerIcon"
+              ></img>
+            </div>
+            <div className={styles.titleContainer}>
+              <component.Title />
+            </div>
+          </div>
+
+          <div className={styles.carruselContainer}>
+            <Carousel />
+          </div>
+        </div>
       </div>
-      <div className={styles.cardListContainer}>
-        {burguerList.map((burger, index) => (
-          <component.ProductCard img={burger} key={index} />
-        ))}
+
+      <div className={styles.middleContainer}>
+        <div className={styles.cardListContainer}>
+          {burguerList.map((burger, index) => (
+            <component.ProductCard img={burger} key={index} />
+          ))}
+        </div>
       </div>
     </div>
   );
